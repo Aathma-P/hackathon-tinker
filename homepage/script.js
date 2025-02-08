a29c312cc7f7712be0e2cbbb98955228e24bca7a
Adithya Shaji <adithyas038@gmail.com>
2025-02-08 21:56:07 +0530

    added sign-up to homepage

--- homepage/script.js
+++
@@ -1,4 +0,0 @@
-// Hide the cookies notice when the user clicks "Accept"
-document.getElementById('accept-cookies').addEventListener('click', function() {
-  document.getElementById('cookies-notice').style.display = 'none';
-});
