b61bb9c8a1b72471bbb07f3f182372d1c52111b3
Adithya Shaji <adithyas038@gmail.com>
2025-02-08 21:44:54 +0530

    Homepage updated

---
+++ homepage/script.js
@@ -0,0 +1,4 @@
+// Hide the cookies notice when the user clicks "Accept"
+document.getElementById('accept-cookies').addEventListener('click', function() {
+  document.getElementById('cookies-notice').style.display = 'none';
+});
